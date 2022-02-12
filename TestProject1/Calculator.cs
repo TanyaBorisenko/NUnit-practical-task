b5daa94cad02af079be99e47dc7c9965b7cdf3f7
Tanya <tanyaborisenko94@gmail.com>
2022-02-12 19:31:02 +0300

    NUnit practical task

---
+++ TestProject1/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TestProject1
+{
+    public class Calculator
+    {
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Div(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return a / b;
+        }
+
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
